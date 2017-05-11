7b5caeeae3403eb7f11a6e83850bfc28ae96fb41
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-11 11:12:53 -0500

    Finished with the exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * Created by
+ * jaimemoncada on 5/11/17.
+ */
+public class StringExercise {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String message = "We don't need no education";
+
+        if (message.equals("We don't need no education")){
+            System.out.println(message);
+        }
+        if (message.equals("We don't need no education")){
+            System.out.println("We don't need no thought control");
+            System.out.println(" ");
+        }
+        if (message.equals("We don't need no education")){
+            System.out.println("Check \"this\" out!, \"s inside of \"s!");
+            System.out.println(" ");
+        }
+        if (message.equals("We don't need no education")){
+            System.out.println("In windows, the main drive is usually C:\\");
+            System.out.println(" ");
+        }
+        if (message.equals("We don't need no education")){
+            System.out.println("I can do backslashes \\, double backslashes \\\\,");
+        }
+        if (message.equals("We don't need no education")){
+            System.out.println("and the amazing triple backslash \\\\\\!");
+        } else System.out.println("We did not expect this :( ");
+
+
+
+
+
+    }
+}
