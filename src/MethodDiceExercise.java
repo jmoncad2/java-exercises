5ddf167d41b9db677dbbd6ef875115d3000e4691
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-14 13:43:29 -0500

    Finished exercise 3

---
+++ src/MethodDiceExercise.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Created by
+ * jaimemoncada on 5/12/17.
+ */
+public class MethodDiceExercise {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String yesNo;
+        int finalRoll = diceRoll(input);
+        System.out.println("Would you like to try again? (Y/N)");
+        yesNo = input.next();
+        if (yesNo.equalsIgnoreCase("y")) {
+            main(args);
+        }
+    }
+
+    public static int diceRoll(Scanner input) {
+        System.out.println("Lets dice roll, enter a number that will be the number of sides in each dice");
+        int sides = input.nextInt();
+        int total;
+
+
+        return total;
+    }
+}
