519d796ea14ed286c683047f943aba6dd3d272f7
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-17 11:27:54 -0500

    Added some new code

--- src/JAVA/personTest.java
+++ src/JAVA/personTest.java
@@ -9,10 +9,19 @@ public class personTest {
         Person person = new Person("James");
         System.out.println(person.getName());
 
+        /*
+        Expected True on both SOUT's
+        But got True on the first and False on the second
+         */
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+        /*
+        Expected True on the SOUT
+         */
         Person person1 = new Person("John");
-        Person person2 = new Person("John");
-        System.out.println(person1.getName().equals(person2.getName()));
+        Person person2 = person1;
         System.out.println(person1 == person2);
-
     }
 }
